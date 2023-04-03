d616b9255d733c1e17b2783c68e8a5879c6e7713
mitchcamza <192mitch88@gmail.com>
2023-04-03 18:21:20 +0200

    Add Course, Teacher, and Student classes to demonstrate aggregation relationship

--- ch02-aggregation-relationship/main.cpp
+++ ch02-aggregation-relationship/main.cpp
@@ -1,9 +1,69 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
-using namespace std;
+class Course {
+public:
+    Course(std::string courseName) : name(courseName) {}
+    std::string getName() { return name; }
+private:
+    std::string name;
+};
 
-int main()
-{
-    cout << "Hello World!" << endl;
+class Student {
+public:
+    Student(std::string studentName) : name(studentName) {}
+    void addCourse(Course* course) { courses.push_back(course); }
+    void printCourses() {
+        std::cout << name << "'s courses:" << std::endl;
+        for (Course* course : courses) {
+            std::cout << " - " << course->getName() << std::endl;
+        }
+    }
+private:
+    std::string name;
+    std::vector<Course*> courses;
+};
+
+class Teacher {
+public:
+    Teacher(std::string teacherName) : name(teacherName) {}
+    void addCourse(Course* course) { courses.push_back(course); }
+    void printCourses() {
+        std::cout << name << "'s courses:" << std::endl;
+        for (Course* course : courses) {
+            std::cout << " - " << course->getName() << std::endl;
+        }
+    }
+private:
+    std::string name;
+    std::vector<Course*> courses;
+};
+
+int main() {
+    Course math("Math");
+    Course english("English");
+    Course history("History");
+
+    Student alice("Alice");
+    alice.addCourse(&math);
+    alice.addCourse(&english);
+
+    Student bob("Bob");
+    bob.addCourse(&math);
+    bob.addCourse(&history);
+
+    Teacher msJones("Ms. Jones");
+    msJones.addCourse(&math);
+    msJones.addCourse(&english);
+
+    Teacher mrSmith("Mr. Smith");
+    mrSmith.addCourse(&history);
+
+    alice.printCourses();
+    bob.printCourses();
+    msJones.printCourses();
+    mrSmith.printCourses();
+
     return 0;
 }
